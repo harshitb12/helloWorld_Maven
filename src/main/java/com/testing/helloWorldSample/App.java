f11eb940145f81b0715e3441f4cdaa664a444999
harshitb12 <72503213+harshitb12@users.noreply.github.com>
2022-09-09 18:11:41 +0530

    maven project

---
+++ src/main/java/com/testing/helloWorldSample/App.java
@@ -0,0 +1,13 @@
+package com.testing.helloWorldSample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
